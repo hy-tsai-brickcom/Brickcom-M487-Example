3a9b61e0599685a18fd4cfb107eed5304e52e503
hy-tsai-brickcom <90504195+hy-tsai-brickcom@users.noreply.github.com>
2021-10-25 16:03:19 +0800

    Add Brickcom-M487-Example

--- pnp/pnp_numaker_iot_m487_dev/pnp_deviceinfo_component.cpp
+++ pnp/pnp_numaker_iot_m487_dev/pnp_deviceinfo_component.cpp
@@ -1,7 +1,23 @@
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+/*
+ * Copyright (c) 2020, Nuvoton Technology Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-// The DTDL for this interface is defined at https://repo.azureiotrepository.com/Models/dtmi:azure:DeviceManagement:DeviceInformation;1?api-version=2020-05-01-preview
+// The DTDL for this interface is defined at:
+//  https://github.com/Azure/iot-plugandplay-models/blob/main/dtmi/azure/devicemanagement/deviceinformation-1.json
 
 // PnP routines
 #include "pnp_deviceinfo_component.h"
